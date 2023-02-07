c1cf9e77efa6be2254a9d7788a083a668c156b08
Sebastian Mottschall <srophkor@gmail.com>
2023-02-07 16:02:07 +0100

    basic setup for app and routes

---
+++ app.js
@@ -0,0 +1,44 @@
+// imports
+const express = require('express');
+const hbs = require('hbs');
+
+
+// init
+const PORT = 3000;
+const app = express();
+
+
+// setup
+app.use(express.static(`${__dirname}/public`));
+app.set('view engine', 'hbs');
+hbs.registerPartials(`${__dirname}/views/partials`);
+
+// routes
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about');
+});
+
+app.get('/works', (req, res)=>{
+    res.render('works');
+});
+
+app.get('/gallery', (req, res)=>{
+    res.render('gallery');
+});
+
+app.get('*', (req, res)=>{
+    res.render('error', {route: req.originalUrl});
+});
+
+
+
+// start server
+app.listen(PORT, (error)=> {
+    if (error) return console.log(error);
+
+    console.log(`Server started. Listening at http://localhost:${PORT}`);
+})
